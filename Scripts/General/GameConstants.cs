e43d26efcd864e3d3568ecdb421d40ecd42098b5
Mainuddin Khaled <mainuddinshahar1393@gmail.com>
2026-07-28 22:57:38 +0600

    Final Commit
    Player Movement and animation complete.

---
+++ Scripts/General/GameConstants.cs
@@ -0,0 +1,12 @@
+public class GameConstants
+{
+    // Animations
+    public const string ANIMATION_IDLE = "Idle";
+    public const string ANIMATION_RUN = "Run";
+
+    // Inputs
+    public const string INPUT_MOVE_LEFT = "MoveLeft";
+    public const string INPUT_MOVE_RIGHT = "MoveRight";
+    public const string INPUT_MOVE_FORWARD = "MoveForward";
+    public const string INPUT_MOVE_BACKWARD = "MoveBackward";
+}
