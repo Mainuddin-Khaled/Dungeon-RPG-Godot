4621e80f258020766a5fd66ff48935952af8815e
Mainuddin Khaled <mainuddinshahar1393@gmail.com>
2026-08-15 22:57:04 +0600

    Fourth Commit

--- Scripts/Characters/Player/Player.cs
+++ Scripts/Characters/Player/Player.cs
@@ -9,14 +9,14 @@ public partial class Player : CharacterBody3D
     [Export] public StateMachine stateMachineNode;
     public Vector2 direction = new();
 
-    public override void _PhysicsProcess(double delta)
-    {
-        Velocity = new(direction.X, 0, direction.Y);
-        Velocity *= 5;
+    // public override void _PhysicsProcess(double delta)
+    // {
+    //     Velocity = new(direction.X, 0, direction.Y);
+    //     Velocity *= 5;
 
-        MoveAndSlide();
-        Flip();
-    }
+    //     MoveAndSlide();
+    //     Flip();
+    // }
 
     public override void _Input(InputEvent @event)
     {
@@ -28,7 +28,7 @@ public partial class Player : CharacterBody3D
         );
     }
 
-    private void Flip()
+    public void Flip()
     {
         bool isNotMovingHorizontally = Velocity.X == 0;
 
--- Scripts/Characters/Player/PlayerDashState.cs
+++ Scripts/Characters/Player/PlayerDashState.cs
@@ -1,13 +1,16 @@
 using Godot;
 using System;
+using System.Reflection.Metadata;
 
 public partial class PlayerDashState : Node
 {
     private Player characterNode;
-
+    [Export] private Timer dashTimerNode;
+    [Export] private float speed = 10;
     public override void _Ready()
     {
         characterNode = GetOwner<Player>();
+        dashTimerNode.Timeout += HandleDashTimeout;
     }
 
     public override void _Notification(int what)
@@ -16,6 +19,29 @@ public partial class PlayerDashState : Node
         if (what == 5001)
         {
             characterNode.animationPlayer.Play(GameConstants.ANIMATION_DASH);
+            characterNode.Velocity = new(
+                characterNode.direction.X, 0, characterNode.direction.Y
+            );
+            if (characterNode.Velocity == Vector3.Zero)
+            {
+                characterNode.Velocity = characterNode.sprite3D.FlipH ?
+                    Vector3.Left :
+                    Vector3.Right ;
+            }
+            characterNode.Velocity *= speed;
+            dashTimerNode.Start();
         }
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        characterNode.MoveAndSlide();
+        characterNode.Flip();
+    }
+
+    private void HandleDashTimeout()
+    {
+        characterNode.Velocity = Vector3.Zero;
+        characterNode.stateMachineNode.SwitchState<PlayerIdleState>();
     }
 }
